0d01dea28d16eeeafd881e274163ce1fbad0f17d
masaosetogawa <ms.job.hunting@gmail.com>
2020-04-07 00:05:38 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  imageData: string[];
+}
+
+class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      imageData: [],
+    };
+  }
+
+  onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+
+    if (files === null) {
+      return;
+    }
+
+    if (files.length < 0) {
+      return;
+    }
+
+    var file = files[0];
+    for 
+    var reader = new FileReader();
+    reader.onload = (event: Event) => {
+      const result: string | ArrayBuffer | null = (event.target as FileReader)
+        .result;
+
+      if (typeof result !== "string") {
+        return;
+      }
+
+      //this.setState({ imageData: result });
+      this.state.imageData.push(result);
+    };
+    reader.readAsDataURL(file);
+
+  };
+  render() {
+    const imageData = this.state.imageData;
+    let preview: JSX.Element = <></>;
+
+    if (imageData != null && typeof imageData === "string") {
+      preview = (
+        <div>
+          <img src={imageData} />
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <input type="file" onChange={this.onChange} multiple></input>
+        {preview}
+      </>
+    );
+  }
 }
 
 export default App;
